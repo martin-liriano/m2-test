ef2a59bdfa464c839f6489c9efdfe129950e5443
martin-liriano <82399525+martin-liriano@users.noreply.github.com>
2023-01-04 14:10:06 -0500

    Update minor_version_update.py

--- minor_version_update.py
+++ minor_version_update.py
@@ -1,26 +1,117 @@
-import re
+import re, sys
 
-file1 = open('version.txt', 'r')
-Lines = file1.readlines()
+# fetch type of upgrade code from bash arguments
+# 0 is Major
+# 1 is Minor
+# 2 is Patch
+typeOfUpgrade = int(sys.argv[1])
 
-stableRegex = re.compile('[0-9]+\.[0-9]+\.')
-updateRegex = re.compile('[0-9]+$')
+# filename dictates where the version file is located that serves as
+# placeholder for all versioning
+filename = 'version.txt'
 
-versionUpdate = ''
-versionStable = ''
-  
-for line in Lines:
-    print(line)
-    versionUpdate = updateRegex.search(line).group()
-    versionStable = stableRegex.search(line).group()
+# updateMajorVersion(Lines) updates the Major version x.0.0
+def updateMajorVersion(Lines) :
+
+    stableRegexString = '\.[0-9]+\.[0-9]+$'
+    updateRegexString = '^[0-9]+'
+
+    stableRegex = re.compile(stableRegexString)
+    updateRegex = re.compile(updateRegexString)
+
+    versionUpdate = ''
+    versionStable = ''
+
+    for line in Lines:
+        versionUpdate = updateRegex.search(line).group()
+        versionStable = stableRegex.search(line).group()
                     
-versionUpdate = int(versionUpdate)
-versionUpdate = versionUpdate + 1
+    versionUpdate = int(versionUpdate)
+    versionUpdate = versionUpdate + 1
 
-version = versionStable + str(versionUpdate)
+    version = str(versionUpdate) + versionStable
+    return version
 
-file2 = open('version.txt', 'w')
+# updateMinorVersion(Lines) updates the minor version 0.x.0
+def updateMinorVersion(Lines) :
 
-file2.write(version)
+    majorVersionString = ''
+    minorVersionString = ''
+    patchVersionString = ''
 
-file2.close()
+    section = 0
+
+    for line in Lines:
+        for c in line:
+            if c == '.' :
+                section = section + 1
+                continue
+            if section == 0 :
+                majorVersionString = majorVersionString + c
+            elif section == 1 :
+                minorVersionString = minorVersionString + c
+            elif section == 2 :
+                patchVersionString = patchVersionString + c
+    
+    minorVersionString = int(minorVersionString)
+    minorVersionString = minorVersionString + 1
+
+    version = majorVersionString + '.' + str(minorVersionString) + '.' + patchVersionString
+    return version
+
+# updatePatchVersion(Lines) updates the Patch version 0.0.x
+def updatePatchVersion(Lines) :
+
+    stableRegexString = '[0-9]+\.[0-9]+\.'
+    updateRegexString = '[0-9]+$'
+
+    stableRegex = re.compile(stableRegexString)
+    print('stableRegex', stableRegex)
+    updateRegex = re.compile(updateRegexString)
+    print('updateRegex', updateRegex)
+
+    versionUpdate = ''
+    versionStable = ''
+
+    for line in Lines:
+        versionUpdate = updateRegex.search(line).group()
+        versionStable = stableRegex.search(line).group()
+                    
+    versionUpdate = int(versionUpdate)
+    versionUpdate = versionUpdate + 1
+
+    version = versionStable + str(versionUpdate)
+    return version
+
+# writeToFile(version) takes in version from update functions and writes it to the version file
+def writeToFile(version) :
+
+    file2 = open('version.txt', 'w')
+
+    file2.write(version)
+
+    file2.close()
+
+# readFile() reads file for use on update functions
+def readFile() :
+    file1 = open('version.txt', 'r')
+    Lines = file1.readlines()
+    return Lines
+
+
+# Extecution steps for above functionality - Steps below
+# 1. Read the file and get acquire the lines
+# 2. Determine type of upgrade that needs to be procesed
+# 3. Process the versioning update
+# 4. Write the update to the file
+
+linesToRead = readFile()
+
+if typeOfUpgrade == 0 :
+    versionToWrite = updateMajorVersion(linesToRead)
+elif typeOfUpgrade == 1 :
+    versionToWrite = updateMinorVersion(linesToRead)
+elif typeOfUpgrade == 2 :
+    versionToWrite = updatePatchVersion(linesToRead)
+
+writeToFile(versionToWrite)
